812b82098a5dd14a0206c5115787a7d6d2b4c4f0
Mauro Valdez <josenova0@gmail.com>
2024-05-01 13:16:35 -0300

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import '@fontsource/kanit/400.css';
+import '@fontsource/kanit/500.css';
+import '@fontsource/kanit/600.css';
+import '@fontsource/lobster-two/400.css'
+import '@fontsource/lobster-two/700.css'
 
 
 function App() {
